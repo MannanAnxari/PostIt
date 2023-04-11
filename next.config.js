d1a669273f6b103f0bed8f65b64cdcab8742de73
MannanAnxari <mannananxari@gmail.com>
2023-04-11 21:44:30 +0500

    .

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    esmExternals: true
-  },
+  // experimental: {
+  //   appDir: true,
+  //   esmExternals: true
+  // },
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
